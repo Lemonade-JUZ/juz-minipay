08d88c0d9254924c0333cb785eb91cf7c5bfba0e
D3Portillo <d3portillo@gmail.com>
2025-06-02 18:49:40 +0200

    feat: add profile info + connect. closes #1

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,32 @@
 import { useWalletAuth } from "@/hooks/wallet"
 import { beautifyAddress } from "@/lib/utils"
 import { Button, useToast } from "@worldcoin/mini-apps-ui-kit-react"
+import { erc20Abi, parseEther } from "viem"
+import { useWriteContract } from "wagmi"
 
+const DEV_ADDRESS = "0xA353557ddfc96325a8ab18E6f6d9c1fC0d7C1eA6"
 export default function Home() {
   const { toast } = useToast()
   const { address, isMiniPay } = useWalletAuth()
 
+  const { writeContract } = useWriteContract()
+
+  function handleSendCUSD() {
+    writeContract({
+      address: "0x765DE816845861e75A25fCA122bb6898B8B1282a",
+      abi: erc20Abi,
+      functionName: "transfer",
+      args: [DEV_ADDRESS, parseEther("0.1")], // Adjust the recipient address and amount as needed
+    })
+  }
+
   return (
-    <main className="flex p-4 h-full flex-col gap-2">
-      <h2 className="font-title text-2xl">Welcome to JUZ</h2>
+    <main className="flex bg-gradient-to-br from-juz-orange/0 via-juz-orange/0 to-juz-orange/7 p-4 pt-6 min-h-full flex-col gap-2">
+      <h2 className="font-title text-xl">Welcome to JUZ</h2>
       <div className="flex-grow" />
 
-      <Button
-        onClick={() =>
-          toast.success({
-            title: "Yay!",
-          })
-        }
-        variant="secondary"
-      >
-        Show success message
+      <Button onClick={handleSendCUSD} variant="secondary">
+        Send cUSD
       </Button>
 
       <Button>
