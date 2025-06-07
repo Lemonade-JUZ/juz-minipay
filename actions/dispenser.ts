67fc970672c1d576d635a6f26ea0bb44357a0f28
D3Portillo <d3portillo@gmail.com>
2025-06-07 02:28:21 +0200

    fix(dispenser.ts): mock was sending 1JUZ instead of user balance. Now we send the expected amount to redeem

--- actions/dispenser.ts
+++ actions/dispenser.ts
@@ -3,22 +3,26 @@
 import { type Address, encodePacked, keccak256, parseEther } from "viem"
 import { privateKeyToAccount } from "viem/accounts"
 import { celo } from "viem/chains"
+import { getPlayerJUZEarned } from "./game"
+import { celoClient } from "@/lib/celo"
 
 import { ABI_DISPENSER } from "@/lib/abis"
-import { celoClient } from "@/lib/celo"
 import { ADDRESS_DISPENSER } from "@/lib/constants"
 
 const account = privateKeyToAccount(process.env.DEV_JUZ_PK as `0x${string}`)
 
 export async function getDispenserPayload(address: Address) {
-  const nonce = await celoClient.readContract({
-    abi: ABI_DISPENSER,
-    functionName: "nonces",
-    address: ADDRESS_DISPENSER,
-    args: [address],
-  })
+  const [points, nonce] = await Promise.all([
+    getPlayerJUZEarned(address),
+    celoClient.readContract({
+      abi: ABI_DISPENSER,
+      functionName: "nonces",
+      address: ADDRESS_DISPENSER,
+      args: [address],
+    }),
+  ])
 
-  const amount = parseEther("1") // Test out 1 JUZ token
+  const amount = parseEther(`${points}`)
   const deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 5) // 5 minutes
   const encoded = encodePacked(
     [
